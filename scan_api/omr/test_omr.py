4b5c17fc67b58a054f928fc0455f33be84de77c3
Samuel Bernardo <samuelbernardo.dev@gmail.com>
2026-05-09 15:06:01 -0300

    refactor: remover arquivo desnecessario

--- scan_api/omr/test_omr.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from omr.services.pipeline import process_image
-
-if __name__ == "__main__":
-    process_image("omr/gabarito_bolhas_vazias.jpg")
